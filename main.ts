f6e04fbd2ef8d639972d296236c8bafe681e7359
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-03 18:11:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+input.onButtonPressed(Button.A, function () {
+    out += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    ball += 1
+})
+input.onButtonPressed(Button.B, function () {
+    strike += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Ball")
+    basic.showNumber(ball)
+    basic.showString("Strike")
+    basic.showNumber(strike)
+    basic.showString("Out")
+    basic.showNumber(out)
+})
+let strike = 0
+let ball = 0
+let out = 0
+out = 0
+ball = 0
+strike = 0
+basic.forever(function () {
+    if (ball == 4) {
+        ball = 0
+        basic.showString("Walk")
+        basic.showString("Walk")
+    }
+    if (out == 3) {
+        ball = 0
+        out = 0
+        strike = 0
+        basic.showString("Switch round")
+        basic.showString("Switch round")
+    }
+    if (strike == 3) {
+        out += 1
+        strike = 0
+        basic.showString("Out")
+        basic.showString("Out")
+    }
+})
