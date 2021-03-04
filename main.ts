db7924ade3d8ec6316c9936d79dacb3a96d249bd
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-04 12:39:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
+// When A is pressed, the variable out increase by 1
 input.onButtonPressed(Button.A, function () {
     out += 1
     basic.showString("Out")
 })
+// When you press A and B at the same time, the variable ball is changing by 1.
 input.onButtonPressed(Button.AB, function () {
     ball += 1
     basic.showString("Ball")
 })
+// When B is pressed it will increase strike by 1
 input.onButtonPressed(Button.B, function () {
     strike += 1
     basic.showString("Strike")
 })
+// When you shake the  microbit it displays the amount of out, strike and ball in the inning.
 input.onGesture(Gesture.Shake, function () {
     basic.showString("Ball")
     basic.showNumber(ball)
@@ -18,12 +22,14 @@ input.onGesture(Gesture.Shake, function () {
     basic.showString("Out")
     basic.showNumber(out)
 })
+// This is because you want all the variable to set to 0
 let strike = 0
 let ball = 0
 let out = 0
 out = 0
 ball = 0
 strike = 0
+// Right here is the brains of the code, so right here, if the variable ball reaches 4, it will clear the variable ball and display walk, if the variable out reaches 3, then it will reset everything to 0 and if the variable reaches 3, it will reset strike to 0 and add 1 to the variable out. 
 basic.forever(function () {
     if (ball == 4) {
         ball = 0
@@ -40,7 +46,4 @@ basic.forever(function () {
         strike = 0
         basic.showString("Out")
     }
-})
-basic.forever(function () {
-	
 })
